b59e1e6af6a7f273e26ed9bf14ce916ad25933ab
Bender Bieger Rodriguez <bieger@rodriguez.wh26.tu-dresden.de>
2012-06-07 16:27:46 +0200

    created vg_get_name and lv_create

--- lxc.py
+++ lxc.py
@@ -39,30 +39,40 @@ MOUNT_CONFIG='configs/mount-options' #this should be a single line which will be
 
 
 
-
-def main():
-    # parse command line options
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], "h", ["help"])
-    except getopt.error, msg:
-        print msg
-        print "for help use --help"
-        sys.exit(2)
-    # process options
-    for o, a in opts:
-        if o in ("-h", "--help"):
-            print __doc__
-            sys.exit(0)
-    # process arguments
-    for arg in args:
-        process(arg) # process() is defined elsewhere
+dev main(argv=None):
+    if argv is None:
+        argv = sys.argv
+        try:
+            try:
+                opts, args = getopt.getopt(argv[1:], "h", ["help"])
+            except getopt.error, msg:
+                raise Usage(msg)
+            # more code, unchanged
+        except Usage, err:
+            print >>sys.stderr, err.msg
+            print >>sys.stderr, "for help use --help"
+    return 2
 
 def lv_create():
-    process = sub.Popen(["lvcreate", "-L" + LV_SIZE, "-n"+ LV_NAME, VG_NAME], shell=False, stdout=sub.PIPE, stderr=sub.PIPE)
-    print process.communicate()
+    p = sub.Popen(["lvcreate", "-L" + LV_SIZE, "-n"+ LV_NAME, VG_NAME], shell=False, stdout=sub.PIPE, stderr=sub.PIPE)
+    try:
+        print p.stdout.read()
+    except:
+        print "Fehler!" + p.stderror.read()
+
+def lv_find_vg():
+    p = sub.Popen(["vgdisplay"], shell=false, stdout=sub.PIPE, stderr=sub.PIPE)
+    try:
+        out = p.stdout.readlines()
+        output = out[1].split()[2]
+        print "VG Name: " + output
+        VG_NAME = output
+    except:
+        print "Fehler! " + p.stderror.read()
+
+
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
     print "hello world"
-    lv_create()
     
